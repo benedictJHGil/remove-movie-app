773ee32e2d1b4f46bf9bc2c0f844e74644bb865c
uniquehan <benedictJHGil@gmail.com>
2023-12-22 16:31:56 +0900

    Add Serverless Functions

--- src/store/movie.js
+++ src/store/movie.js
@@ -1,4 +1,5 @@
 import { Store } from '../core/uniqueHan'
+import fetch from "node-fetch";
 
 const store = new Store({
   searchText: '',
@@ -19,7 +20,13 @@ export const searchMovies = async page => {
     store.state.message = ''
   }
   try {
-    const res = await fetch(`https://www.omdbapi.com?apikey=27f371d5&s=${store.state.searchText}&page=${page}`)
+    const res = await fetch('/api/movie', {
+      method: 'POST',
+      body: JSON.stringify({
+        title: store.state.searchText,
+        page
+      })
+    })
     const { Search, totalResults, Response, Error } = await res.json()
     if (Response === 'True') {
       store.state.movies = [
@@ -40,7 +47,12 @@ export const searchMovies = async page => {
 
 export const getMovieDetails = async id => {
   try {
-    const res = await fetch(`https://www.omdbapi.com?apikey=27f371d5&i=${id}&plot=full`)
+    const res = await fetch('/api/movie', {
+      method: 'POST',
+      body: JSON.stringify({
+        id
+      })
+    })
     store.state.movie = await res.json()
   } catch (error) {
     console.log('getMovieDetails error: ', error)
